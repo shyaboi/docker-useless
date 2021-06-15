2bde3ba01d35941559ffdb89d0a28137a9b6a16f
shyaboi <ian.shane.sears@gmail.com>
2021-06-14 21:44:24 -0700

    making little changes for main deploy

--- docker.sh
+++ docker.sh
@@ -9,8 +9,7 @@ docker build -t shyaboi/ufront .
 docker network create useless-net
 
 docker run -d --net useless-net -p 27017:27017 --name mong \
-      -e MONGO_INITDB_ROOT_USERNAME=useless \
-      -e MONGO_INITDB_ROOT_PASSWORD=uP%4055 \
+#check .env
       mongo
 # start the node app containers
 # DB_H="144.126.222.91"
